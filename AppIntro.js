8176cae45f851e08fa2f21644300f2b05cb0682e
Marcus Andersson <systemvetaren@gmail.com>
2017-09-29 16:14:23 +0200

    changed so this package will work with react 16.^

--- AppIntro.js
+++ AppIntro.js
@@ -1,5 +1,6 @@
 import assign from 'assign-deep';
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import {
   StatusBar,
   StyleSheet,
