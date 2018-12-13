94fbd8b22e0f4b594bc6f9ad2e4b4b09382b1dc5
Fabian Carlos <fabian.pow@gmail.com>
2018-12-13 11:13:31 -0300

    fix proptypes

--- Example/AppIntro.js
+++ Example/AppIntro.js
@@ -1,5 +1,6 @@
 import assign from 'assign-deep';
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   StatusBar,
   StyleSheet,
